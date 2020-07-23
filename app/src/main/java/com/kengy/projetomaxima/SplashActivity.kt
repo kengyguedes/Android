6884a6d43cdc3ae2ea5d1c43265efbc22a0ade8f
kengy <kengyysk8@gmail.com>
2020-07-23 19:26:35 -0300

    Layout tela menu principal

---
+++ app/src/main/java/com/kengy/projetomaxima/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.kengy.projetomaxima
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+class SplashScreen : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+
+    }
+
+}
